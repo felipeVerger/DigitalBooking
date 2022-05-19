09678cefa6013061c2e60158d49941e93e9dbc9a
MatiasFz <matias_fernandez92@hotmail.com>
2022-05-19 20:49:29 -0300

    probando

---
+++ back/ProyectInt/src/main/java/model/Categoria.java
@@ -0,0 +1,50 @@
+package model;
+
+public class Categoria {
+
+
+
+    private Long id;
+    private String titulo;
+    private String descripcion;
+    private String imagen;
+
+    public Categoria(Long id, String titulo, String descripcion, String imagen) {
+        this.id = id;
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.imagen = imagen;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+}
