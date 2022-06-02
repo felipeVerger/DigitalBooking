bfaa134d468c26eca206a3081d769544a89f9008
MatiasFz <matias_fernandez92@hotmail.com>
2022-06-02 18:33:29 -0300

    Agregando relacion de tablas

--- back/ProyectInt/src/main/java/com/proyecto/proyectInt/model/Product.java
+++ back/ProyectInt/src/main/java/com/proyecto/proyectInt/model/Product.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.List;
 
 
 @Getter
@@ -42,6 +43,18 @@ public class Product {
 
     @Column
     private String cancellationPolicy;
+
+    @ManyToOne
+    private Category category;
+
+    @ManyToMany
+    private List<Feature> features;
+
+    @OneToMany
+    private List<Image> images;
+
+    @ManyToOne
+    private City city;
 
 
 
