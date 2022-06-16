741fba170743a4e17c2851078175049f324f5bd4
felipeverger <felipeverger123@gmail.com>
2022-06-16 12:18:43 -0300

    Successful message responsive y icono agregado

--- front/reserva-hoteles/src/components/SuccessfulMessagePage/index.js
+++ front/reserva-hoteles/src/components/SuccessfulMessagePage/index.js
@@ -2,16 +2,18 @@ import React from 'react'
 import {
     Container,
     Box,
+    Logo,
     Gratitude,
     MessageText,
     Button,
 } from './SuccessfulMessage'
 import { Link } from 'react-router-dom'
 
-const Message = ({message}) => {
+const Message = () => {
   return (
     <Container>
         <Box>
+            <Logo src={require("../../assets/successful.png")} />
             <Gratitude>¡Muchas gracias!</Gratitude>
             <MessageText>Su reserva se ha realizado con éxito.</MessageText>
             <Link to={'/'}>
