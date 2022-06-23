11964ceb56096f9f8cc106d1459e60d7e191160b
felipeverger <felipeverger123@gmail.com>
2022-06-23 12:28:52 -0300

    agregando filtro por ciudad y catgoria con datos de la api

--- front/reserva-hoteles/src/components/Products/index.js
+++ front/reserva-hoteles/src/components/Products/index.js
@@ -4,7 +4,8 @@ import ProductCard from './ProductsCard'
 import { FilterContext } from '../../context/filter-context'
 import { useLocation } from 'react-router-dom'
 import useFetch from '../../hooks/useFetch';
-import Loading from '../Loading'
+import Loading from '../Loading';
+import { fetchData, options } from '../../utils/fetchData';
 
 const URL_API = 'http://localhost:8080/products/findAll'
 const URL_API_CITY = 'http://localhost:8080/products/city/'
@@ -13,37 +14,57 @@ const Recomendaciones = () => {
     const {filter} = useContext(FilterContext);
     const locationPath = useLocation().pathname;
 
-    const myHeaders = new Headers();
-    myHeaders.append("Authorization", "Basic dXNlcjpnMTBCb29raW5n");
+    const [products, setProducts] = useState([]);
 
-    const { data: products, loading, error } = useFetch(URL_API, {
-        method: 'GET',
-        headers: myHeaders,
-        redirect: 'follow'
-    })
+  useEffect(() => {
+    const fetchProductsData = async () => { 
+      const productUrl = 'http://localhost:8080/products/findAll';
+      const productByCityUrl = 'http://localhost:8080/products/city/'
+      const productByCategoryUrl = 'http://localhost:8080/products/category/'
+      const productByCityAndDateUrl = 'http://localhost:8080/products/date/'
+
+      if(filter && filter[1] === 'city' && locationPath === '/productsList'){
+        let productsByCity = await fetchData(productByCityUrl + filter[0], options);
+        setProducts(productsByCity);
+      } else if (filter && filter[1] === 'category' && locationPath === '/productsList'){
+        let productsByCategory = await fetchData(productByCategoryUrl + filter[0], options);
+        setProducts(productsByCategory);
+      } else {
+        const productsData = await fetchData(productUrl, options);
+        setProducts(productsData);
+      }
+    }
+    fetchProductsData();
+  }, []) 
+
+
+    
 
     /* A ternary operator that checks if the products array has more than 6 elements, if it does, it
     sorts the array randomly and then slices the first 6 elements, if it doesn't, it returns the
     products array. */
-    const randomProducts = products && products.length > 6 ? products.sort(() => Math.random() - 0.5).slice(0, 6) : products; 
+    const randomProducts = products && products.length > 6 ? products.sort(() => Math.random() - 0.5).slice(0, 6) : products;
+    const filteredProducts = locationPath === '/productsList' ? products : randomProducts; 
 
     /* Filtering the products based on the filter context. */
-    const filteredArray = filter ? products && filter[1] === 'category' && locationPath === '/productsList' ? products.filter((product) => product.category.title === filter[0]) 
-    : products && filter[1] === 'city' && locationPath === '/productsList' ? products.filter((product) => product.city.name + ', ' + product.city.country === filter[0]) : randomProducts : randomProducts;
+    // const filteredArray = filter ? products && filter[1] === 'category' && locationPath === '/productsList' ? products.filter((product) => product.category.title === filter[0]) 
+    // : products && filter[1] === 'city' && locationPath === '/productsList' ? products.filter((product) => product.city.name + ', ' + product.city.country === filter[0]) : randomProducts : randomProducts;
 
-
-  return loading ? <Loading/> : !filter && locationPath === '/productsList' ? <ErrorMessage>No se encontraron resultados</ErrorMessage> : 
+  if (!filteredProducts.length) {
+    return <Loading/>
+  }
+  return !filter && locationPath === '/productsList' ? <ErrorMessage>No se encontraron resultados</ErrorMessage> : 
   (
     <Body>
         <Block>
-            <Title>{locationPath === '/' ? 'Recomendaciones' : `${filter[0]}: ${filteredArray.length} alojamientos encontrados`}</Title>
+            <Title>{locationPath === '/' ? 'Recomendaciones' : `${filter[0]}: ${randomProducts.length} alojamientos encontrados`}</Title>
             <RecommendationContainer>
                 {
-                   filteredArray.map((product) => {
+                   filteredProducts.map((product) => {
                         return (
                             <ProductCard
                                 key={product.id}
-                                img={product.images && product.images[1] ? product.images[1].url : ""}
+                                img={product.images && product.images[0] ? product.images[0].url : ""}
                                 category={product.category.title}
                                 title={product.name}
                                 location={product.address}
