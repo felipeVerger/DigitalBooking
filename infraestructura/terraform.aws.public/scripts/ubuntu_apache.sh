f908c9333bddbac2d46a373f3dc91045a356bca5
Natalia <varesinatalia@gmail.com>
2022-06-27 01:41:11 -0300

    elimino carpeta de archivos sin usar

--- infraestructura/terraform.aws.public/scripts/ubuntu_apache.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-apt update -y
-apt install apache2 -y
-systemctl start apache2
-systemctl enable apache2
-
-echo "[${ENV}] Hello World from $(hostname -f)" > /var/www/html/index.html
-
