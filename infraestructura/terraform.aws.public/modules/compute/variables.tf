0238a5188ba3952768f22239c881e8a9fff7cda3
Natalia <varesinatalia@gmail.com>
2022-06-27 01:34:00 -0300

    agrego lb en front y back para agregar ha y bastion con ip elastica

---
+++ infraestructura/terraform.aws.public/modules/compute/variables.tf
@@ -0,0 +1,48 @@
+variable "env" {}
+variable "project" {}
+
+# Subnet
+variable "subnet_public" {}
+variable "subnet_frontend" {}
+variable "subnet_backend" {}
+variable "subnet_database" {}
+
+# Security group
+variable "sg_bastion" {}
+variable "sg_http" {}
+variable "sg_private" {}
+
+# Elastic load balancing
+variable "lb_tg_arn_frontend" {}
+variable "lb_tg_arn_backend" {}
+
+# Instance key pair
+variable "public_key_pair_bastion" {
+  sensitive = true
+  default   = "0821-g10"
+}
+variable "public_key_pair_project" {
+  sensitive = true
+  default   = "0821-g10"
+}
+
+# Instance type
+variable "instance_type_bastion" {
+  type    = string
+  default = "t2.micro"
+}
+
+variable "instance_type_frontend" {
+  type    = string
+  default = "t2.micro"
+}
+
+variable "instance_type_backend" {
+  type    = string
+  default = "t2.micro"
+}
+
+variable "instance_type_database" {
+  type    = string
+  default = "t2.micro"
+}
