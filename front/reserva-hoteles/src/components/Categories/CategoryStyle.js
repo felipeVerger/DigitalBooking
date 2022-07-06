a04f820590d421bcc26eda2913b34b8d27bdf600
felipeverger <felipeverger123@gmail.com>
2022-07-06 11:24:08 -0300

    imagenes de categorias acomodadas

--- front/reserva-hoteles/src/components/Categories/CategoryStyle.js
+++ front/reserva-hoteles/src/components/Categories/CategoryStyle.js
@@ -40,6 +40,7 @@ export const CategoryInfo = styled.p`
 export const Image = styled.img`
   min-width: 100%;
   max-width: 100%;
+  height: 300px;
   border-radius: 10px 10px 0px 0px;
   /* @media screen and (max-width: 768px) {
         width: 100%;
